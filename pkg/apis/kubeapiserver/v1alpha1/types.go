856c06e047c6d65cd53b6f8b5438c7c6240c28e3
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2018-08-27 16:27:15 +0200

    Add initial types

---
+++ pkg/apis/kubeapiserver/v1alpha1/types.go
@@ -0,0 +1,53 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+
+	operatorsv1alpha1api "github.com/openshift/api/operator/v1alpha1"
+)
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// KubeApiserverConfig provides information to configure kube-apiserver
+type KubeApiserverConfig struct {
+	metav1.TypeMeta `json:",inline"`
+}
+
+// +genclient
+// +genclient:nonNamespaced
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// KubeApiserverOperatorConfig provides information to configure an operator to manage kube-apiserver.
+type KubeApiserverOperatorConfig struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata" protobuf:"bytes,1,opt,name=metadata"`
+
+	Spec   KubeApiserverOperatorConfigSpec   `json:"spec" protobuf:"bytes,2,opt,name=spec"`
+	Status KubeApiserverOperatorConfigStatus `json:"status" protobuf:"bytes,3,opt,name=status"`
+}
+
+type KubeApiserverOperatorConfigSpec struct {
+	operatorsv1alpha1api.OperatorSpec `json:",inline" protobuf:"bytes,1,opt,name=operatorSpec"`
+
+	// kubeApiserverConfig holds a sparse config that the user wants for this component.  It only needs to be the overrides from the defaults
+	// it will end up overlaying in the following order:
+	// 1. hardcoded default
+	// 2. this config
+	KubeApiserverConfig runtime.RawExtension `json:"kubeApiserverConfig" protobuf:"bytes,2,opt,name=kubeApiserverConfig"`
+}
+
+type KubeApiserverOperatorConfigStatus struct {
+	operatorsv1alpha1api.OperatorStatus `json:",inline" protobuf:"bytes,1,opt,name=operatorStatus"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// KubeApiserverOperatorConfigList is a collection of items
+type KubeApiserverOperatorConfigList struct {
+	metav1.TypeMeta `json:",inline"`
+	// Standard object's metadata.
+	metav1.ListMeta `json:"metadata,omitempty" protobuf:"bytes,1,opt,name=metadata"`
+	// Items contains the items
+	Items []KubeApiserverOperatorConfig `json:"items" protobuf:"bytes,2,rep,name=items"`
+}
