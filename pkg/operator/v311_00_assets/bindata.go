9644f71276766f584bee25e09898d639125822fd
David Eads <deads@redhat.com>
2018-10-02 10:41:13 -0400

    only run on master nodes

--- pkg/operator/v311_00_assets/bindata.go
+++ pkg/operator/v311_00_assets/bindata.go
@@ -330,9 +330,8 @@ spec:
       - name: serving-cert
         secret:
           secretName: serving-cert
-
-
-
+      nodeSelector:
+        node-role.kubernetes.io/master: ""
 `)
 
 func v3110KubeApiserverDeploymentYamlBytes() ([]byte, error) {
