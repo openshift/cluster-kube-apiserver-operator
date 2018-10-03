f39629ce737ffde1f9fd6f190c6e43d2b37941a9
Maciej Szulik <maszulik@redhat.com>
2018-10-03 13:06:59 +0200

    Fixes basic yaml linting test

--- pkg/test/assets_test.go
+++ pkg/test/assets_test.go
@@ -3,28 +3,27 @@ package test
 import (
 	"testing"
 
+	"github.com/ghodss/yaml"
+
 	"github.com/openshift/cluster-kube-apiserver-operator/cmd/cluster-kube-apiserver-operator/render"
 	"github.com/openshift/library-go/pkg/assets"
-	yaml "gopkg.in/yaml.v2"
 )
 
 func TestYamlCorrectness(t *testing.T) {
 	readAllYaml("../../manifests/", t)
 	readAllYaml("../../bindata/", t)
 }
-
-func readAllYaml(path string, t *testing.T) error {
+func readAllYaml(path string, t *testing.T) {
 	manifests, err := assets.New(path, render.Config{}, assets.OnlyYaml)
 	if err != nil {
-		return err
+		t.Errorf("Unexpected error reading manifests from %s: %v", path, err)
 	}
 	t.Logf("Found %d manifests in %s", len(manifests), path)
 	for _, m := range manifests {
 		contents := make(map[string]interface{})
 		t.Logf("Checking %s...", m.Name)
 		if err := yaml.Unmarshal(m.Data, &contents); err != nil {
-			t.Errorf("Got unexpected error unmarshaling %s: %v", m.Name, err)
+			t.Errorf("Unexpected error unmarshaling %s: %v", m.Name, err)
 		}
 	}
-	return nil
 }
