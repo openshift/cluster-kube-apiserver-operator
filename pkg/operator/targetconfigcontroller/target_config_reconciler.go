86fd70f352ed79f8f83c1c508825db84504c12e4
David Eads <deads@redhat.com>
2019-01-17 11:40:17 -0500

    allow configuring log level of operand

--- pkg/operator/targetconfigcontroller/target_config_reconciler.go
+++ pkg/operator/targetconfigcontroller/target_config_reconciler.go
@@ -208,7 +208,18 @@ func managePod(client coreclientv1.ConfigMapsGetter, recorder events.Recorder, o
 	if len(imagePullSpec) > 0 {
 		required.Spec.Containers[0].Image = imagePullSpec
 	}
-	required.Spec.Containers[0].Args = append(required.Spec.Containers[0].Args, fmt.Sprintf("-v=%d", 2))
+	switch operatorConfig.Spec.LogLevel {
+	case operatorv1.Normal:
+		required.Spec.Containers[0].Args = append(required.Spec.Containers[0].Args, fmt.Sprintf("-v=%d", 2))
+	case operatorv1.Debug:
+		required.Spec.Containers[0].Args = append(required.Spec.Containers[0].Args, fmt.Sprintf("-v=%d", 4))
+	case operatorv1.Trace:
+		required.Spec.Containers[0].Args = append(required.Spec.Containers[0].Args, fmt.Sprintf("-v=%d", 6))
+	case operatorv1.TraceAll:
+		required.Spec.Containers[0].Args = append(required.Spec.Containers[0].Args, fmt.Sprintf("-v=%d", 8))
+	default:
+		required.Spec.Containers[0].Args = append(required.Spec.Containers[0].Args, fmt.Sprintf("-v=%d", 2))
+	}
 
 	configMap := resourceread.ReadConfigMapV1OrDie(v311_00_assets.MustAsset("v3.11.0/kube-apiserver/pod-cm.yaml"))
 	configMap.Data["pod.yaml"] = resourceread.WritePodV1OrDie(required)
