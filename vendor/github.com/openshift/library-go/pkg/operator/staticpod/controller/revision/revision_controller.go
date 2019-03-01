6892540e1f03b9aa75b67a6597e84c6c2647c9f3
Michal Fojtik <mfojtik@redhat.com>
2019-03-01 13:08:39 +0100

    bump(library-go): improve logging of what causes a static pod rollout

--- vendor/github.com/openshift/library-go/pkg/operator/staticpod/controller/revision/revision_controller.go
+++ vendor/github.com/openshift/library-go/pkg/operator/staticpod/controller/revision/revision_controller.go
@@ -2,6 +2,7 @@ package revision
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/golang/glog"
@@ -117,6 +118,10 @@ func (c RevisionController) createRevisionIfNeeded(operatorSpec *operatorv1.Stat
 		Status: operatorv1.ConditionFalse,
 	}
 	if _, updated, updateError := v1helpers.UpdateStaticPodStatus(c.operatorConfigClient, v1helpers.UpdateStaticPodConditionFn(cond), func(operatorStatus *operatorv1.StaticPodOperatorStatus) error {
+		if operatorStatus.LatestAvailableRevision == nextRevision {
+			glog.Warningf("revision %d is unexpectedly already the latest available revision. This is a possible race!", nextRevision)
+			return fmt.Errorf("conflicting latestAvailableRevision %d", operatorStatus.LatestAvailableRevision)
+		}
 		operatorStatus.LatestAvailableRevision = nextRevision
 		return nil
 	}); updateError != nil {
@@ -134,6 +139,7 @@ func nameFor(name string, revision int32) string {
 
 // isLatestRevisionCurrent returns whether the latest revision is up to date and an optional reason
 func (c RevisionController) isLatestRevisionCurrent(revision int32) (bool, string) {
+	configChanges := []string{}
 	for _, cm := range c.configMaps {
 		requiredData := map[string]string{}
 		existingData := map[string]string{}
@@ -153,9 +159,14 @@ func (c RevisionController) isLatestRevisionCurrent(revision int32) (bool, strin
 			existingData = existing.Data
 		}
 		if !equality.Semantic.DeepEqual(existingData, requiredData) {
-			return false, fmt.Sprintf("configmap/%s has changed", cm.Name)
+			if glog.V(4) {
+				glog.Infof("configmap %q changes for revision %d: %s", cm.Name, revision, resourceapply.JSONPatch(existing, required))
+			}
+			configChanges = append(configChanges, fmt.Sprintf("configmap/%s has changed", cm.Name))
 		}
 	}
+
+	secretChanges := []string{}
 	for _, s := range c.secrets {
 		requiredData := map[string][]byte{}
 		existingData := map[string][]byte{}
@@ -175,8 +186,15 @@ func (c RevisionController) isLatestRevisionCurrent(revision int32) (bool, strin
 			existingData = existing.Data
 		}
 		if !equality.Semantic.DeepEqual(existingData, requiredData) {
-			return false, fmt.Sprintf("secret/%s has changed", s.Name)
+			if glog.V(4) {
+				glog.Infof("secret %q changes for revision %d: %s", s.Name, revision, resourceapply.JSONPatch(existing, required))
+			}
+			secretChanges = append(secretChanges, fmt.Sprintf("secret/%s has changed", s.Name))
 		}
+	}
+
+	if len(secretChanges) > 0 || len(configChanges) > 0 {
+		return false, strings.Join(append(secretChanges, configChanges...), ",")
 	}
 
 	return true, ""
