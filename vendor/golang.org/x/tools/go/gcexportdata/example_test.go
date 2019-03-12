114b9c6e2fe7beef1b30342fe289c9ceb88df5c1
Mike Dame <mdame@redhat.com>
2019-03-12 12:45:41 -0400

    bump(*): library-go check for revision status 0

--- vendor/golang.org/x/tools/go/gcexportdata/example_test.go
+++ vendor/golang.org/x/tools/go/gcexportdata/example_test.go
@@ -3,7 +3,6 @@
 // license that can be found in the LICENSE file.
 
 // +build go1.7
-// +build gc
 
 package gcexportdata_test
 
@@ -70,15 +69,14 @@ func ExampleRead() {
 // ExampleNewImporter demonstrates usage of NewImporter to provide type
 // information for dependencies when type-checking Go source code.
 func ExampleNewImporter() {
-	const src = `package myrpc
+	const src = `package twopi
 
-// choosing a package that doesn't change across releases
-import "net/rpc"
+import "math"
 
-const defaultRPCPath = rpc.DefaultRPCPath
+const TwoPi = 2 * math.Pi
 `
 	fset := token.NewFileSet()
-	f, err := parser.ParseFile(fset, "myrpc.go", src, 0)
+	f, err := parser.ParseFile(fset, "twopi.go", src, 0)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -86,34 +84,32 @@ const defaultRPCPath = rpc.DefaultRPCPath
 	packages := make(map[string]*types.Package)
 	imp := gcexportdata.NewImporter(fset, packages)
 	conf := types.Config{Importer: imp}
-	pkg, err := conf.Check("myrpc", fset, []*ast.File{f}, nil)
+	pkg, err := conf.Check("twopi", fset, []*ast.File{f}, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// object from imported package
-	pi := packages["net/rpc"].Scope().Lookup("DefaultRPCPath")
+	pi := packages["math"].Scope().Lookup("Pi")
 	fmt.Printf("const %s.%s %s = %s // %s\n",
 		pi.Pkg().Path(),
 		pi.Name(),
 		pi.Type(),
 		pi.(*types.Const).Val(),
-		slashify(fset.Position(pi.Pos())),
-	)
+		slashify(fset.Position(pi.Pos())))
 
 	// object in source package
-	twopi := pkg.Scope().Lookup("defaultRPCPath")
+	twopi := pkg.Scope().Lookup("TwoPi")
 	fmt.Printf("const %s %s = %s // %s\n",
 		twopi.Name(),
 		twopi.Type(),
 		twopi.(*types.Const).Val(),
-		slashify(fset.Position(twopi.Pos())),
-	)
+		slashify(fset.Position(twopi.Pos())))
 
 	// Output:
 	//
-	// const net/rpc.DefaultRPCPath untyped string = "/_goRPC_" // $GOROOT/src/net/rpc/server.go:146:1
-	// const defaultRPCPath untyped string = "/_goRPC_" // myrpc.go:6:7
+	// const math.Pi untyped float = 3.14159 // $GOROOT/src/math/const.go:11:1
+	// const TwoPi untyped float = 6.28319 // twopi.go:5:7
 }
 
 func slashify(posn token.Position) token.Position {
