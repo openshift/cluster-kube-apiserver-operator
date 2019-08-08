70eac2e4ab1d9078ca60499dad5dea72737242ac
Stanislav Laznicka <slaznick@redhat.com>
2019-08-08 16:38:10 +0200

    bump (*) to observe Proxy status instead of spec

--- vendor/github.com/openshift/library-go/pkg/operator/configobserver/proxy/observe_proxy.go
+++ vendor/github.com/openshift/library-go/pkg/operator/configobserver/proxy/observe_proxy.go
@@ -72,15 +72,15 @@ func (f *observeProxyFlags) ObserveProxyConfig(genericListers configobserver.Lis
 func proxyToMap(proxy *configv1.Proxy) map[string]string {
 	proxyMap := map[string]string{}
 
-	if noProxy := proxy.Spec.NoProxy; len(noProxy) > 0 {
+	if noProxy := proxy.Status.NoProxy; len(noProxy) > 0 {
 		proxyMap["NO_PROXY"] = noProxy
 	}
 
-	if httpProxy := proxy.Spec.HTTPProxy; len(httpProxy) > 0 {
+	if httpProxy := proxy.Status.HTTPProxy; len(httpProxy) > 0 {
 		proxyMap["HTTP_PROXY"] = httpProxy
 	}
 
-	if httpsProxy := proxy.Spec.HTTPSProxy; len(httpsProxy) > 0 {
+	if httpsProxy := proxy.Status.HTTPSProxy; len(httpsProxy) > 0 {
 		proxyMap["HTTPS_PROXY"] = httpsProxy
 	}
 
