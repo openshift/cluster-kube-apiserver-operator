c036e18fffffa28d7470d6c2723f8d2dc37a7521
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2019-10-11 15:16:16 +0200

    cleans up Listers struct

--- pkg/operator/configobservation/interfaces.go
+++ pkg/operator/configobservation/interfaces.go
@@ -5,21 +5,23 @@ import (
 	"k8s.io/client-go/tools/cache"
 
 	configlistersv1 "github.com/openshift/client-go/config/listers/config/v1"
-	"github.com/openshift/library-go/pkg/operator/configobserver/cloudprovider"
+	"github.com/openshift/library-go/pkg/operator/configobserver"
 	"github.com/openshift/library-go/pkg/operator/resourcesynccontroller"
 )
 
-var _ cloudprovider.InfrastructureLister = Listers{}
+// must satisfy configobserver.Listers interface which is passed to the config observer functions. It is expected to be hard-cast to the "correct" type.
+var _ configobserver.Listers = Listers{}
 
+// Listers a struct that holds a bunch of listers for various resources
 type Listers struct {
-	APIServerLister       configlistersv1.APIServerLister
-	AuthConfigLister      configlistersv1.AuthenticationLister
-	FeatureGateLister_    configlistersv1.FeatureGateLister
-	InfrastructureLister_ configlistersv1.InfrastructureLister
-	ImageConfigLister     configlistersv1.ImageLister
-	NetworkLister         configlistersv1.NetworkLister
-	ProxyLister_          configlistersv1.ProxyLister
-	SchedulerLister       configlistersv1.SchedulerLister
+	APIServerLister      configlistersv1.APIServerLister
+	AuthConfigLister     configlistersv1.AuthenticationLister
+	FeatureGateLister    configlistersv1.FeatureGateLister
+	InfrastructureLister configlistersv1.InfrastructureLister
+	ImageConfigLister    configlistersv1.ImageLister
+	NetworkLister        configlistersv1.NetworkLister
+	ProxyLister          configlistersv1.ProxyLister
+	SchedulerLister      configlistersv1.SchedulerLister
 
 	OpenshiftEtcdEndpointsLister corelistersv1.EndpointsLister
 	ConfigmapLister              corelistersv1.ConfigMapLister
@@ -29,22 +31,10 @@ type Listers struct {
 	PreRunCachesSynced []cache.InformerSynced
 }
 
-func (l Listers) FeatureGateLister() configlistersv1.FeatureGateLister {
-	return l.FeatureGateLister_
-}
-
-func (l Listers) InfrastructureLister() configlistersv1.InfrastructureLister {
-	return l.InfrastructureLister_
+func (l Listers) PreRunHasSynced() []cache.InformerSynced {
+	return l.PreRunCachesSynced
 }
 
 func (l Listers) ResourceSyncer() resourcesynccontroller.ResourceSyncer {
 	return l.ResourceSync
-}
-
-func (l Listers) ProxyLister() configlistersv1.ProxyLister {
-	return l.ProxyLister_
-}
-
-func (l Listers) PreRunHasSynced() []cache.InformerSynced {
-	return l.PreRunCachesSynced
 }
