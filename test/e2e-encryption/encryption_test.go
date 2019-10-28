cdaaa07ef95d361d5296ceba3b7d12892420681a
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2019-10-28 12:59:44 +0100

    uses e2e encryption code from library-go

--- test/e2e-encryption/encryption_test.go
+++ test/e2e-encryption/encryption_test.go
@@ -6,22 +6,29 @@ import (
 
 	"github.com/stretchr/testify/require"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/rand"
 
 	configv1 "github.com/openshift/api/config/v1"
-	"github.com/openshift/cluster-kube-apiserver-operator/test/library/encryption"
+	"github.com/openshift/cluster-kube-apiserver-operator/pkg/operator/operatorclient"
+	operatorencryption "github.com/openshift/cluster-kube-apiserver-operator/test/library/encryption"
+	"github.com/openshift/library-go/test/library/encryption"
 )
 
 func TestEncryptionTypeIdentity(t *testing.T) {
 	e := encryption.NewE(t)
-	clientSet := encryption.SetAndWaitForEncryptionType(e, configv1.EncryptionTypeIdentity)
-	encryption.AssertSecretsAndConfigMaps(e, clientSet, configv1.EncryptionTypeIdentity)
+	ns := operatorclient.GlobalMachineSpecifiedConfigNamespace
+	labelSelector := "encryption.apiserver.operator.openshift.io/component" + "=" + operatorclient.TargetNamespace
+	clientSet := encryption.SetAndWaitForEncryptionType(e, configv1.EncryptionTypeIdentity, operatorencryption.DefaultTargetGRs, ns, labelSelector)
+	operatorencryption.AssertSecretsAndConfigMaps(e, clientSet, configv1.EncryptionTypeIdentity, ns, labelSelector)
 }
 
 func TestEncryptionTypeUnset(t *testing.T) {
 	e := encryption.NewE(t)
-	clientSet := encryption.SetAndWaitForEncryptionType(e, "")
-	encryption.AssertSecretsAndConfigMaps(e, clientSet, configv1.EncryptionTypeIdentity)
+	ns := operatorclient.GlobalMachineSpecifiedConfigNamespace
+	labelSelector := "encryption.apiserver.operator.openshift.io/component" + "=" + operatorclient.TargetNamespace
+	clientSet := encryption.SetAndWaitForEncryptionType(e, "", operatorencryption.DefaultTargetGRs, ns, labelSelector)
+	operatorencryption.AssertSecretsAndConfigMaps(e, clientSet, configv1.EncryptionTypeIdentity, ns, labelSelector)
 }
 
 func TestEncryptionTurnOnAndOff(t *testing.T) {
@@ -31,27 +38,27 @@ func TestEncryptionTurnOnAndOff(t *testing.T) {
 	}{
 		{name: "CreateAndStoreSecretOfLife", testFunc: func(t *testing.T) {
 			e := encryption.NewE(t)
-			encryption.CreateAndStoreSecretOfLife(e, encryption.GetClients(e))
+			encryption.CreateAndStoreSecretOfLife(e, encryption.GetClients(e), operatorclient.GlobalMachineSpecifiedConfigNamespace)
 		}},
-		{name: "OnAESCBC", testFunc: encryption.TestEncryptionTypeAESCBC},
+		{name: "OnAESCBC", testFunc: operatorencryption.TestEncryptionTypeAESCBC},
 		{name: "AssertSecretOfLifeEncrypted", testFunc: func(t *testing.T) {
 			e := encryption.NewE(t)
-			encryption.AssertSecretOfLifeEncrypted(e, encryption.GetClients(e), encryption.SecretOfLife(e))
+			encryption.AssertSecretOfLifeEncrypted(e, encryption.GetClients(e), encryption.SecretOfLife(e, operatorclient.GlobalMachineSpecifiedConfigNamespace))
 		}},
 		{name: "OffIdentity", testFunc: TestEncryptionTypeIdentity},
 		{name: "AssertSecretOfLifeNotEncrypted", testFunc: func(t *testing.T) {
 			e := encryption.NewE(t)
-			encryption.AssertSecretOfLifeNotEncrypted(e, encryption.GetClients(e), encryption.SecretOfLife(e))
+			encryption.AssertSecretOfLifeNotEncrypted(e, encryption.GetClients(e), encryption.SecretOfLife(e, operatorclient.GlobalMachineSpecifiedConfigNamespace))
 		}},
-		{name: "OnAESCBCSecond", testFunc: encryption.TestEncryptionTypeAESCBC},
+		{name: "OnAESCBCSecond", testFunc: operatorencryption.TestEncryptionTypeAESCBC},
 		{name: "AssertSecretOfLifeEncryptedSecond", testFunc: func(t *testing.T) {
 			e := encryption.NewE(t)
-			encryption.AssertSecretOfLifeEncrypted(e, encryption.GetClients(e), encryption.SecretOfLife(e))
+			encryption.AssertSecretOfLifeEncrypted(e, encryption.GetClients(e), encryption.SecretOfLife(e, operatorclient.GlobalMachineSpecifiedConfigNamespace))
 		}},
 		{name: "OffIdentitySecond", testFunc: TestEncryptionTypeIdentity},
 		{name: "AssertSecretOfLifeNotEncryptedSecond", testFunc: func(t *testing.T) {
 			e := encryption.NewE(t)
-			encryption.AssertSecretOfLifeNotEncrypted(e, encryption.GetClients(e), encryption.SecretOfLife(e))
+			encryption.AssertSecretOfLifeNotEncrypted(e, encryption.GetClients(e), encryption.SecretOfLife(e, operatorclient.GlobalMachineSpecifiedConfigNamespace))
 		}},
 	}
 
@@ -69,26 +76,40 @@ func TestEncryptionTurnOnAndOff(t *testing.T) {
 // then it forces a key rotation by setting the "encyrption.Reason" in the operator's configuration file
 func TestEncryptionRotation(t *testing.T) {
 	// TODO: dump events, conditions in case of an failure for all scenarios
+
+	// test data
+	ns := operatorclient.GlobalMachineSpecifiedConfigNamespace
+	labelSelector := "encryption.apiserver.operator.openshift.io/component" + "=" + operatorclient.TargetNamespace
+
 	// step 1: create the secret of life
 	e := encryption.NewE(t)
 	clientSet := encryption.GetClients(e)
-	encryption.CreateAndStoreSecretOfLife(e, encryption.GetClients(e))
+	encryption.CreateAndStoreSecretOfLife(e, encryption.GetClients(e), ns)
 
 	// step 2: run encryption aescbc scenario
-	encryption.TestEncryptionTypeAESCBC(t)
+	operatorencryption.TestEncryptionTypeAESCBC(t)
 
 	// step 3: take samples
-	rawEncryptedSecretOfLifeWithKey1 := encryption.GetRawSecretOfLife(e, clientSet)
+	rawEncryptedSecretOfLifeWithKey1 := encryption.GetRawSecretOfLife(e, clientSet, ns)
 
 	// step 4: force key rotation and wait for migration to complete
-	lastMigratedKeyMeta, err := encryption.GetLastKeyMeta(clientSet.Kube)
+	lastMigratedKeyMeta, err := encryption.GetLastKeyMeta(clientSet.Kube, ns, labelSelector)
 	require.NoError(e, err)
-	require.NoError(e, encryption.ForceKeyRotation(e, clientSet.Operator, fmt.Sprintf("test-key-rotation-%s", rand.String(4))))
-	encryption.WaitForNextMigratedKey(e, clientSet.Kube, lastMigratedKeyMeta)
-	encryption.AssertSecretsAndConfigMaps(e, clientSet, configv1.EncryptionTypeAESCBC)
+	require.NoError(e, encryption.ForceKeyRotation(e, func(raw []byte) error {
+		operatorClient := operatorencryption.GetOperator(t)
+		apiServerOperator, err := operatorClient.Get("cluster", metav1.GetOptions{})
+		if err != nil {
+			return err
+		}
+		apiServerOperator.Spec.UnsupportedConfigOverrides.Raw = raw
+		_, err = operatorClient.Update(apiServerOperator)
+		return err
+	}, fmt.Sprintf("test-key-rotation-%s", rand.String(4))))
+	encryption.WaitForNextMigratedKey(e, clientSet.Kube, lastMigratedKeyMeta, operatorencryption.DefaultTargetGRs, ns, labelSelector)
+	operatorencryption.AssertSecretsAndConfigMaps(e, clientSet, configv1.EncryptionTypeAESCBC, ns, labelSelector)
 
 	// step 5: verify if the secret of life was encrypted with a different key (step 2 vs step 4)
-	rawEncryptedSecretOfLifeWithKey2 := encryption.GetRawSecretOfLife(e, clientSet)
+	rawEncryptedSecretOfLifeWithKey2 := encryption.GetRawSecretOfLife(e, clientSet, ns)
 	if rawEncryptedSecretOfLifeWithKey1 == rawEncryptedSecretOfLifeWithKey2 {
 		t.Errorf("expected the secret of life to has a differnt content after a key rotation,\ncontentBeforeRotation %s\ncontentAfterRotation %s", rawEncryptedSecretOfLifeWithKey1, rawEncryptedSecretOfLifeWithKey2)
 	}
--- test/library/encryption/assertion.go
+++ test/library/encryption/assertion.go
@@ -1,167 +1,38 @@
 package encryption
 
 import (
-	"bytes"
-	"context"
-	"encoding/hex"
-	"fmt"
-	"strings"
 	"testing"
-	"time"
 
-	"github.com/coreos/etcd/clientv3"
 	"github.com/stretchr/testify/require"
 
-	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/client-go/kubernetes"
 
 	configv1 "github.com/openshift/api/config/v1"
+	library "github.com/openshift/library-go/test/library/encryption"
 )
 
-var protoEncodingPrefix = []byte{0x6b, 0x38, 0x73, 0x00}
-
-var defaultTargetGRs = []schema.GroupResource{
+var DefaultTargetGRs = []schema.GroupResource{
 	{Group: "", Resource: "secrets"},
 	{Group: "", Resource: "configmaps"},
 }
 
-const (
-	jsonEncodingPrefix           = "{"
-	protoEncryptedDataPrefix     = "k8s:enc:"
-	aesCBCTransformerPrefixV1    = "k8s:enc:aescbc:v1:"
-	secretboxTransformerPrefixV1 = "k8s:enc:secretbox:v1:"
-)
-
-func AssertSecretsAndConfigMaps(t testing.TB, clientSet ClientSet, expectedMode configv1.EncryptionType) {
+func AssertSecretsAndConfigMaps(t testing.TB, clientSet library.ClientSet, expectedMode configv1.EncryptionType, namespace, labelSelector string) {
 	t.Helper()
 	assertSecrets(t, clientSet.Etcd, string(expectedMode))
 	assertConfigMaps(t, clientSet.Etcd, string(expectedMode))
-	assertLastMigratedKey(t, clientSet.Kube)
+	library.AssertLastMigratedKey(t, clientSet.Kube, DefaultTargetGRs, namespace, labelSelector)
 }
 
-func AssertSecretOfLifeNotEncrypted(t testing.TB, clientSet ClientSet, secretOfLife *corev1.Secret) {
-	t.Helper()
-	rawSecretValue := GetRawSecretOfLife(t, clientSet)
-	if !strings.Contains(rawSecretValue, string(secretOfLife.Data["quote"])) {
-		t.Errorf("The secret received from etcd doesn't have %q, content of the secret (etcd) %s", string(secretOfLife.Data["quote"]), rawSecretValue)
-	}
-}
-
-func AssertSecretOfLifeEncrypted(t testing.TB, clientSet ClientSet, secretOfLife *corev1.Secret) {
-	t.Helper()
-	rawSecretValue := GetRawSecretOfLife(t, clientSet)
-	if strings.Contains(rawSecretValue, string(secretOfLife.Data["quote"])) {
-		t.Errorf("The secret received from etcd have %q (plain text), content of the secret (etcd) %s", string(secretOfLife.Data["quote"]), rawSecretValue)
-	}
-}
-
-func assertSecrets(t testing.TB, etcdClient EtcdClient, expectedMode string) {
+func assertSecrets(t testing.TB, etcdClient library.EtcdClient, expectedMode string) {
 	t.Logf("Checking if all Secrets where encrypted/decrypted for %q mode", expectedMode)
-	totalSecrets, err := verifyResources(t, etcdClient, "/kubernetes.io/secrets/", expectedMode)
+	totalSecrets, err := library.VerifyResources(t, etcdClient, "/kubernetes.io/secrets/", expectedMode)
 	t.Logf("Verified %d Secrets, err %v", totalSecrets, err)
 	require.NoError(t, err)
 }
 
-func assertConfigMaps(t testing.TB, etcdClient EtcdClient, expectedMode string) {
+func assertConfigMaps(t testing.TB, etcdClient library.EtcdClient, expectedMode string) {
 	t.Logf("Checking if all ConfigMaps where encrypted/decrypted for %q mode", expectedMode)
-	totalConfigMaps, err := verifyResources(t, etcdClient, "/kubernetes.io/configmaps/", expectedMode)
+	totalConfigMaps, err := library.VerifyResources(t, etcdClient, "/kubernetes.io/configmaps/", expectedMode)
 	t.Logf("Verified %d ConfigMaps, err %v", totalConfigMaps, err)
 	require.NoError(t, err)
-}
-
-func assertLastMigratedKey(t testing.TB, kubeClient kubernetes.Interface) {
-	t.Helper()
-	expectedGRs := defaultTargetGRs
-	t.Logf("Checking if the last migrated key was used to encrypt %v", expectedGRs)
-	lastMigratedKeyMeta, err := GetLastKeyMeta(kubeClient)
-	require.NoError(t, err)
-	if len(lastMigratedKeyMeta.Name) == 0 {
-		t.Log("Nothing to check no new key was created")
-		return
-	}
-
-	if len(expectedGRs) != len(lastMigratedKeyMeta.Migrated) {
-		t.Errorf("Wrong number of migrated resources for %q key, expected %d, got %d", lastMigratedKeyMeta.Name, len(expectedGRs), len(lastMigratedKeyMeta.Migrated))
-	}
-
-	for _, expectedGR := range expectedGRs {
-		if !hasResource(expectedGR, lastMigratedKeyMeta.Migrated) {
-			t.Errorf("%q wasn't used to encrypt %v, only %v", lastMigratedKeyMeta.Name, expectedGR, lastMigratedKeyMeta.Migrated)
-		}
-	}
-}
-
-func verifyResources(t testing.TB, etcdClient EtcdClient, etcdKeyPreifx, expectedMode string) (int, error) {
-	timeout, cancel := context.WithTimeout(context.Background(), 10*time.Minute)
-	defer cancel()
-
-	resp, err := etcdClient.Get(timeout, etcdKeyPreifx, clientv3.WithPrefix())
-	switch {
-	case err != nil:
-		return 0, fmt.Errorf("failed to list prefix %s: %v", etcdKeyPreifx, err)
-	case resp.Count == 0 || len(resp.Kvs) == 0:
-		return 0, fmt.Errorf("empty list response for prefix %s: %+v", etcdKeyPreifx, resp)
-	case resp.More:
-		return 0, fmt.Errorf("incomplete list response for prefix %s: %+v", etcdKeyPreifx, resp)
-	}
-
-	for _, keyValue := range resp.Kvs {
-		if err := verifyPrefixForRawData(expectedMode, keyValue.Value); err != nil {
-			return 0, fmt.Errorf("key %s failed check: %v\n%s", keyValue.Key, err, hex.Dump(keyValue.Value))
-		}
-	}
-
-	return len(resp.Kvs), nil
-}
-
-func verifyPrefixForRawData(expectedMode string, data []byte) error {
-	if len(data) == 0 {
-		return fmt.Errorf("empty data")
-	}
-
-	conditionToStr := func(condition bool) string {
-		if condition {
-			return "encrypted"
-		}
-		return "unencrypted"
-	}
-
-	expectedEncrypted := true
-	if expectedMode == "identity" {
-		expectedMode = "identity-proto"
-		expectedEncrypted = false
-	}
-
-	actualMode, isEncrypted := encryptionModeFromEtcdValue(data)
-	if expectedEncrypted != isEncrypted {
-		return fmt.Errorf("unexpected encrypted state, expected data to be %q but was %q with mode %q", conditionToStr(expectedEncrypted), conditionToStr(isEncrypted), actualMode)
-	}
-	if actualMode != expectedMode {
-		return fmt.Errorf("unexpected encryption mode %q, expected %q, was data encrypted/decrypted with a wrong key", actualMode, expectedMode)
-	}
-
-	return nil
-}
-
-func encryptionModeFromEtcdValue(data []byte) (string, bool) {
-	isEncrypted := bytes.HasPrefix(data, []byte(protoEncryptedDataPrefix)) // all encrypted data has this prefix
-	return func() string {
-		switch {
-		case hasPrefixAndTrailingData(data, []byte(aesCBCTransformerPrefixV1)): // AES-CBC has this prefix
-			return "aescbc"
-		case hasPrefixAndTrailingData(data, []byte(secretboxTransformerPrefixV1)): // Secretbox has this prefix
-			return "secretbox"
-		case hasPrefixAndTrailingData(data, []byte(jsonEncodingPrefix)): // unencrypted json data has this prefix
-			return "identity-json"
-		case hasPrefixAndTrailingData(data, protoEncodingPrefix): // unencrypted protobuf data has this prefix
-			return "identity-proto"
-		default:
-			return "unknown" // this should never happen
-		}
-	}(), isEncrypted
-}
-
-func hasPrefixAndTrailingData(data, prefix []byte) bool {
-	return bytes.HasPrefix(data, prefix) && len(data) > len(prefix)
 }
