9c2f9e679f0d53d3d65cff1db5af58d4215f1b8c
Maru Newby <marun@redhat.com>
2020-01-27 11:40:56 -0800

    Move bindata config to its own path
    cd bindata/v4.1.0
    mkdir config
    mv kube-apiserver/{config-overrides.yaml,defaultconfig.yaml} config

--- pkg/operator/targetconfigcontroller/targetconfigcontroller.go
+++ pkg/operator/targetconfigcontroller/targetconfigcontroller.go
@@ -217,8 +217,8 @@ func createTargetConfig(c TargetConfigController, recorder events.Recorder, oper
 
 func manageKubeAPIServerConfig(client coreclientv1.ConfigMapsGetter, recorder events.Recorder, operatorSpec *operatorv1.StaticPodOperatorSpec) (*corev1.ConfigMap, bool, error) {
 	configMap := resourceread.ReadConfigMapV1OrDie(v410_00_assets.MustAsset("v4.1.0/kube-apiserver/cm.yaml"))
-	defaultConfig := v410_00_assets.MustAsset("v4.1.0/kube-apiserver/defaultconfig.yaml")
-	configOverrides := v410_00_assets.MustAsset("v4.1.0/kube-apiserver/config-overrides.yaml")
+	defaultConfig := v410_00_assets.MustAsset("v4.1.0/config/defaultconfig.yaml")
+	configOverrides := v410_00_assets.MustAsset("v4.1.0/config/config-overrides.yaml")
 	specialMergeRules := map[string]resourcemerge.MergeFunc{
 		".oauthConfig": RemoveConfig,
 	}
