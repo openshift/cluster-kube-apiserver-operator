0d6cf2d64ab0f7a4f4828474c61dc5a4b05b756d
Tomas Nozicka <tnozicka@gmail.com>
2020-04-15 17:47:41 +0200

    Fix kube-apiserver-to-kubelet-signer's refresh date

--- pkg/operator/certrotationcontroller/certrotationcontroller.go
+++ pkg/operator/certrotationcontroller/certrotationcontroller.go
@@ -174,7 +174,7 @@ func newCertRotationController(
 			Namespace:              operatorclient.OperatorNamespace,
 			Name:                   "kube-apiserver-to-kubelet-signer",
 			Validity:               1 * 365 * defaultRotationDay, // this comes from the installer
-			Refresh:                8 * 365 * defaultRotationDay, // this means we effectively do not rotate
+			Refresh:                292 * defaultRotationDay,
 			RefreshOnlyWhenExpired: refreshOnlyWhenExpired,
 			Informer:               kubeInformersForNamespaces.InformersFor(operatorclient.OperatorNamespace).Core().V1().Secrets(),
 			Lister:                 kubeInformersForNamespaces.InformersFor(operatorclient.OperatorNamespace).Core().V1().Secrets().Lister(),
