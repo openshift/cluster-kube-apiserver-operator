85dad572cb4700cc2728fbcfb78f7a9d4cc133b5
Emily Moss <emoss@redhat.com>
2021-04-05 12:39:55 -0700

    change path for pathGetter

--- pkg/operator/starter.go
+++ pkg/operator/starter.go
@@ -93,7 +93,7 @@ func RunOperator(ctx context.Context, controllerContext *controllercmd.Controlle
 		return err
 	}
 
-	auditPolicyPahGetter, err := libgoassets.NewAuditPolicyPathGetter("/var/run/configmaps/audit")
+	auditPolicyPahGetter, err := libgoassets.NewAuditPolicyPathGetter("/etc/kubernetes/static-pod-resources/configmaps/kube-apiserver-audit-policies")
 	if err != nil {
 		return err
 	}
