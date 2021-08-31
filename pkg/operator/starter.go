b5c52ca8315079568e33710a3a7406c958d2203c
Jan Chaloupka <jchaloup@redhat.com>
2021-08-31 12:25:10 +0200

    Deploy PDB to prevent more than one replica going unavailable
    In case master nodes are drained to quickly in a row there's no
    waiting until instances go back to available. So it's possible
    for two or more replicas to go unavailable which may lead
    to all instances going unavailable.

--- pkg/operator/starter.go
+++ pkg/operator/starter.go
@@ -148,6 +148,7 @@ func RunOperator(ctx context.Context, controllerContext *controllercmd.Controlle
 			"assets/kube-apiserver/apiserver.openshift.io_apirequestcount.yaml",
 			"assets/kube-apiserver/storage-version-migration-flowschema.yaml",
 			"assets/kube-apiserver/storage-version-migration-prioritylevelconfiguration.yaml",
+			"assets/kube-apiserver/pdb.yaml",
 			"assets/alerts/api-usage.yaml",
 			"assets/alerts/audit-errors.yaml",
 			"assets/alerts/cpu-utilization.yaml",
