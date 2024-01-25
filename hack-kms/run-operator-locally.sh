bd4045825d86bd4049c4674a87122b409346af2e
Swarup Ghosh <swghosh@redhat.com>
2024-01-25 19:26:38 +0530

    Now run KMSv2 e2e on a running AWS cluster with one command: bash hack-kms/run-operator-locally.sh

--- hack-kms/run-operator-locally.sh
+++ hack-kms/run-operator-locally.sh
@@ -9,6 +9,15 @@ oc scale -n openshift-cluster-version deploy cluster-version-operator --replicas
 # scale down operator running on cluster
 oc scale -n openshift-kube-apiserver-operator deploy kube-apiserver-operator --replicas=0
 
+# enable encryption on the cluster
+# this looks like will enable aesgcm encryption for etcd
+# but due to our patched implementation
+# this will use KMS instead of AES-GCM
+# 
+# NOTE: This is an important step, without this step only kms plugin
+# will be added as container to kube-apiserver pod but never be used!!
+oc patch --type=merge apiserver/cluster --type=merge -p='{"spec":{"encryption":{"type":"aesgcm"}}}'
+
 OPERATOR_ENVS=$(oc get deploy kube-apiserver-operator -n openshift-kube-apiserver-operator -o json | jq '.spec.template.spec.containers[0].env')
 
 export IMAGE=$(echo "${OPERATOR_ENVS[@]}" | jq '.[] | select(.name=="IMAGE") | .value' -r)
