6cc9355c09e64aaad6494a3bc2d12658cc2706d1
wangke19 <kewang@redhat.com>
2025-10-06 03:31:11 +0800

    Copy dependencies for tests based on OCP CLI

---
+++ test/extended/util/gomega_helpers.go
@@ -0,0 +1,46 @@
+package util
+
+import (
+	"fmt"
+	"os"
+	"reflect"
+
+	"github.com/onsi/gomega/types"
+)
+
+var secureMatchesMessage = fmt.Sprintf(
+	"For security reasons we cannot print the compared values. If you need to debug this error you can `export %s=yes` and then the values will be printed",
+	"ENABLE_DEBUG_LOG_FOR_SECURE_MATCHER",
+)
+
+// SecureMatcher it will not print the compared values when the matcher fails
+type SecureMatcher struct {
+	securedMatcher types.GomegaMatcher
+}
+
+// Match checks it the condition with the given type has the right value in the given field.
+func (matcher *SecureMatcher) Match(actual interface{}) (success bool, err error) {
+	return matcher.securedMatcher.Match(actual)
+}
+
+// FailureMessage returns the message in case of successful match
+func (matcher *SecureMatcher) FailureMessage(actual interface{}) (message string) {
+	if _, enabled := os.LookupEnv("ENABLE_DEBUG_LOG_FOR_SECURE_MATCHER"); enabled {
+		return matcher.securedMatcher.FailureMessage(actual)
+	}
+
+	matcherType := reflect.TypeOf(matcher.securedMatcher).String()
+
+	return fmt.Sprintf("%s did NOT match!! ", matcherType) + secureMatchesMessage
+}
+
+// NegatedFailureMessage returns the message in case of failed match
+func (matcher *SecureMatcher) NegatedFailureMessage(actual interface{}) (message string) {
+	if _, enabled := os.LookupEnv("ENABLE_DEBUG_LOG_FOR_SECURE_MATCHER"); enabled {
+		return matcher.securedMatcher.NegatedFailureMessage(actual)
+	}
+
+	matcherType := reflect.TypeOf(matcher.securedMatcher).String()
+
+	return fmt.Sprintf("%s matched, but should NOT match!! ", matcherType) + secureMatchesMessage
+}
