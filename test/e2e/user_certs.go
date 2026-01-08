92e3b59878d3d7cba3fd6ab2d16e48784d324e37
gangwgr <rgangwar@redhat.com>
2026-01-08 19:06:48 +0530

    Updating individual names

--- test/e2e/user_certs.go
+++ test/e2e/user_certs.go
@@ -19,6 +19,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
 	clientcorev1 "k8s.io/client-go/kubernetes/typed/core/v1"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/util/cert"
 	"k8s.io/client-go/util/keyutil"
 
@@ -32,8 +33,150 @@ import (
 )
 
 var _ = g.Describe("[sig-api-machinery] kube-apiserver operator", func() {
-	g.It("[Operator][Serial][Timeout:40m] TestNamedCertificates", func() {
-		testNamedCertificates(g.GinkgoTB())
+	g.Context("[Operator][Serial][Timeout:40m] TestNamedCertificates", func() {
+		var (
+			kubeConfig   *rest.Config
+			kubeClient   *clientcorev1.CoreV1Client
+			configClient *configclient.ConfigV1Client
+			rootCA       *test.CryptoMaterials
+
+			testCertInfoById map[string]*testCertInfo
+
+			defaultServingCertSerialNumber           string
+			localhostServingCertSerialNumber         string
+			serviceServingCertSerialNumber           string
+			externalLoadBalancerCertSerialNumber     string
+			internalLoadBalancerCertSerialNumber     string
+			localhostRecoveryServingCertSerialNumber string
+
+			serviceIPServerName            string
+			externalLoadBalancerServerName string
+			internalLoadBalancerServerName string
+		)
+
+		g.BeforeEach(func() {
+			var err error
+
+			// create a root certificate authority crypto materials
+			rootCA = test.NewCertificateAuthorityCertificate(g.GinkgoTB(), nil)
+
+			// details of the test certs that will be created, keyed by an string "id"
+			testCertInfoById = map[string]*testCertInfo{
+				"one":   newTestCertInfo(g.GinkgoTB(), "one", rootCA, "one.test"),
+				"two":   newTestCertInfo(g.GinkgoTB(), "two", rootCA, "two.test"),
+				"three": newTestCertInfo(g.GinkgoTB(), "three", rootCA, "three.test", "four.test"),
+			}
+
+			// initialize clients
+			kubeConfig, err = test.NewClientConfigForTest()
+			require.NoError(g.GinkgoTB(), err)
+			kubeClient, err = clientcorev1.NewForConfig(kubeConfig)
+			require.NoError(g.GinkgoTB(), err)
+			configClient, err = configclient.NewForConfig(kubeConfig)
+			require.NoError(g.GinkgoTB(), err)
+
+			// before starting our test make sure that all apis are on the same revision
+			// a previous test might have triggered a new revision and failed
+			testlibraryapi.WaitForAPIServerToStabilizeOnTheSameRevision(g.GinkgoTB(), kubeClient.Pods(operatorclient.TargetNamespace))
+
+			// create secrets for named serving certificates
+			for _, info := range testCertInfoById {
+				_, err := createTLSSecret(kubeClient, "openshift-config", info.secretName, info.crypto.PrivateKey, info.crypto.Certificate)
+				require.NoError(g.GinkgoTB(), err)
+			}
+
+			// configure named certificates
+			_, err = updateAPIServerClusterConfigSpec(configClient, func(apiServer *configv1.APIServer) {
+				apiServer.Spec.ServingCerts.NamedCertificates = append(
+					apiServer.Spec.ServingCerts.NamedCertificates,
+					configv1.APIServerNamedServingCert{ServingCertificate: configv1.SecretNameReference{Name: testCertInfoById["one"].secretName}},
+					configv1.APIServerNamedServingCert{ServingCertificate: configv1.SecretNameReference{Name: testCertInfoById["two"].secretName}},
+					configv1.APIServerNamedServingCert{ServingCertificate: configv1.SecretNameReference{Name: testCertInfoById["three"].secretName}},
+				)
+			})
+			require.NoError(g.GinkgoTB(), err)
+
+			// get serial number of default serving certificate
+			// the default is actually the service-network so that we can easily recognize it in error messages for bad names
+			defaultServingCertSerialNumber = serialNumberOfCertificateFromSecretOrFail(g.GinkgoTB(), kubeClient, "openshift-kube-apiserver", "service-network-serving-certkey")
+			localhostServingCertSerialNumber = serialNumberOfCertificateFromSecretOrFail(g.GinkgoTB(), kubeClient, "openshift-kube-apiserver", "localhost-serving-cert-certkey")
+			serviceServingCertSerialNumber = serialNumberOfCertificateFromSecretOrFail(g.GinkgoTB(), kubeClient, "openshift-kube-apiserver", "service-network-serving-certkey")
+			externalLoadBalancerCertSerialNumber = serialNumberOfCertificateFromSecretOrFail(g.GinkgoTB(), kubeClient, "openshift-kube-apiserver", "external-loadbalancer-serving-certkey")
+			internalLoadBalancerCertSerialNumber = serialNumberOfCertificateFromSecretOrFail(g.GinkgoTB(), kubeClient, "openshift-kube-apiserver", "internal-loadbalancer-serving-certkey")
+			localhostRecoveryServingCertSerialNumber = serialNumberOfCertificateFromSecretOrFail(g.GinkgoTB(), kubeClient, "openshift-kube-apiserver", "localhost-recovery-serving-certkey")
+
+			// get server names that need to be computed
+			serviceIPServerName = getKubernetesServiceClusterIPOrFail(g.GinkgoTB(), kubeClient)
+			externalLoadBalancerServerName = getExternalAPIServiceHostNameOrFail(g.GinkgoTB(), configClient)
+			internalLoadBalancerServerName = getInternalAPIServiceHostNameOrFail(g.GinkgoTB(), configClient)
+
+			g.GinkgoTB().Logf("default serial: %v", defaultServingCertSerialNumber)
+			g.GinkgoTB().Logf("localhost serial: %v", localhostServingCertSerialNumber)
+			g.GinkgoTB().Logf("service serial: %v", serviceServingCertSerialNumber)
+			g.GinkgoTB().Logf("external lb serial: %v", externalLoadBalancerCertSerialNumber)
+			g.GinkgoTB().Logf("internal lb serial: %v", internalLoadBalancerCertSerialNumber)
+			g.GinkgoTB().Logf("localhost recovery serial: %v", localhostRecoveryServingCertSerialNumber)
+			g.GinkgoTB().Logf("external api service hostname: %v", externalLoadBalancerServerName)
+			g.GinkgoTB().Logf("internal api service hostname: %v", internalLoadBalancerServerName)
+
+			// wait until a new version has been rolled out with the new configuration
+			testlibraryapi.WaitForAPIServerToStabilizeOnTheSameRevision(g.GinkgoTB(), kubeClient.Pods(operatorclient.TargetNamespace))
+		})
+
+		g.AfterEach(func() {
+			// cleanup secrets
+			for _, info := range testCertInfoById {
+				err := deleteSecret(kubeClient, "openshift-config", info.secretName)
+				require.NoError(g.GinkgoTB(), err)
+			}
+
+			// cleanup named certificates configuration
+			_, err := updateAPIServerClusterConfigSpec(configClient, func(apiserver *configv1.APIServer) {
+				removeNamedCertificatesBySecretName(apiserver,
+					testCertInfoById["one"].secretName,
+					testCertInfoById["two"].secretName,
+					testCertInfoById["three"].secretName,
+				)
+			})
+			assert.NoError(g.GinkgoTB(), err)
+		})
+
+		testCertificate := func(serverName string, expectedSerialNumber string) {
+			// since not all nodes are guaranteed to be updated, give each test case up to a minute to find the right one
+			err := wait.PollImmediate(time.Second, time.Minute, func() (bool, error) {
+				// connect to apiserver using a custom ServerName and examine the returned certificate's
+				// serial number to determine if the expected serving certificate was returned.
+				serialNumber, err := getReturnedCertSerialNumber(kubeConfig.Host, serverName)
+				require.NoError(g.GinkgoTB(), err)
+				return expectedSerialNumber == serialNumber, nil
+			})
+			require.NoError(g.GinkgoTB(), err)
+		}
+
+		g.DescribeTable("should return correct certificate for server name",
+			func(getServerName func() string, getExpectedSerial func() string) {
+				testCertificate(getServerName(), getExpectedSerial())
+			},
+			g.Entry("User one.test", func() string { return "one.test" }, func() string { return testCertInfoById["one"].crypto.Certificate.SerialNumber.String() }),
+			g.Entry("User two.test", func() string { return "two.test" }, func() string { return testCertInfoById["two"].crypto.Certificate.SerialNumber.String() }),
+			g.Entry("User three.test", func() string { return "three.test" }, func() string { return testCertInfoById["three"].crypto.Certificate.SerialNumber.String() }),
+			g.Entry("User four.test", func() string { return "four.test" }, func() string { return testCertInfoById["three"].crypto.Certificate.SerialNumber.String() }),
+			g.Entry("Service kubernetes", func() string { return "kubernetes" }, func() string { return serviceServingCertSerialNumber }),
+			g.Entry("Service kubernetes.default", func() string { return "kubernetes.default" }, func() string { return serviceServingCertSerialNumber }),
+			g.Entry("Service kubernetes.default.svc", func() string { return "kubernetes.default.svc" }, func() string { return serviceServingCertSerialNumber }),
+			g.Entry("Service kubernetes.default.svc.cluster.local", func() string { return "kubernetes.default.svc.cluster.local" }, func() string { return serviceServingCertSerialNumber }),
+			g.Entry("Service openshift", func() string { return "openshift" }, func() string { return serviceServingCertSerialNumber }),
+			g.Entry("Service openshift.default", func() string { return "openshift.default" }, func() string { return serviceServingCertSerialNumber }),
+			g.Entry("Service openshift.default.svc", func() string { return "openshift.default.svc" }, func() string { return serviceServingCertSerialNumber }),
+			g.Entry("Service openshift.default.svc.cluster.local", func() string { return "openshift.default.svc.cluster.local" }, func() string { return serviceServingCertSerialNumber }),
+			g.Entry("ServiceIP", func() string { return serviceIPServerName }, func() string { return defaultServingCertSerialNumber }),
+			g.Entry("Localhost localhost", func() string { return "localhost" }, func() string { return localhostServingCertSerialNumber }),
+			g.Entry("Localhost 127.0.0.1", func() string { return "127.0.0.1" }, func() string { return defaultServingCertSerialNumber }),
+			g.Entry("Localhost localhost-recovery", func() string { return "localhost-recovery" }, func() string { return localhostRecoveryServingCertSerialNumber }),
+			g.Entry("ExternalLoadBalancerHostname", func() string { return externalLoadBalancerServerName }, func() string { return externalLoadBalancerCertSerialNumber }),
+			g.Entry("InternalLoadBalancerHostname", func() string { return internalLoadBalancerServerName }, func() string { return internalLoadBalancerCertSerialNumber }),
+			g.Entry("UnknownServerHostname", func() string { return "unknown.test" }, func() string { return defaultServingCertSerialNumber }),
+		)
 	})
 })
 
