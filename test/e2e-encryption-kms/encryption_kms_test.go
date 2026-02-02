83a4156120fb60a1b9480326996cef286d7581b4
Arda Güçlü <aguclu@redhat.com>
2026-02-02 19:37:01 +0300

    Update kms tests

--- test/e2e-encryption-kms/encryption_kms_test.go
+++ test/e2e-encryption-kms/encryption_kms_test.go
@@ -1,20 +1,47 @@
 package e2e_encryption_kms
 
 import (
+	"context"
+	"fmt"
 	"testing"
+
+	configv1 "github.com/openshift/api/config/v1"
+	"github.com/openshift/cluster-kube-apiserver-operator/pkg/operator/operatorclient"
+	operatorencryption "github.com/openshift/cluster-kube-apiserver-operator/test/library/encryption"
+	library "github.com/openshift/library-go/test/library/encryption"
+	librarykms "github.com/openshift/library-go/test/library/encryption/kms"
 )
 
 // TestKMSEncryptionOnOff tests KMS encryption on/off cycle.
 // This test:
 // 1. Deploys the mock KMS plugin
-// 2. Enables KMS encryption
-// 3. Verifies secrets are encrypted
-// 4. Disables encryption (Identity)
-// 5. Verifies secrets are not encrypted
-// 6. Re-enables KMS encryption
-// 7. Cleans up
-//
-// TODO: Implement full KMS encryption test once the CI job is validated.
+// 2. Creates a test secret (SecretOfLife)
+// 3. Enables KMS encryption
+// 4. Verifies secret is encrypted
+// 5. Disables encryption (Identity)
+// 6. Verifies secret is NOT encrypted
+// 7. Re-enables KMS encryption
+// 8. Verifies secret is encrypted again
+// 9. Disables encryption (Identity) again
+// 10. Verifies secret is NOT encrypted again
+// 11. Cleans up the KMS plugin
 func TestKMSEncryptionOnOff(t *testing.T) {
-	t.Log("KMS encryption on/off test placeholder - CI job validation")
+	t.Cleanup(librarykms.DeployUpstreamMockKMSPlugin(context.Background(), t, library.GetClients(t).Kube, librarykms.WellKnownUpstreamMockKMSPluginNamespace, librarykms.WellKnownUpstreamMockKMSPluginImage))
+	library.TestEncryptionTurnOnAndOff(t, library.OnOffScenario{
+		BasicScenario: library.BasicScenario{
+			Namespace:                       operatorclient.GlobalMachineSpecifiedConfigNamespace,
+			LabelSelector:                   "encryption.apiserver.operator.openshift.io/component" + "=" + operatorclient.TargetNamespace,
+			EncryptionConfigSecretName:      fmt.Sprintf("encryption-config-%s", operatorclient.TargetNamespace),
+			EncryptionConfigSecretNamespace: operatorclient.GlobalMachineSpecifiedConfigNamespace,
+			OperatorNamespace:               operatorclient.OperatorNamespace,
+			TargetGRs:                       operatorencryption.DefaultTargetGRs,
+			AssertFunc:                      operatorencryption.AssertSecretsAndConfigMaps,
+		},
+		CreateResourceFunc:             operatorencryption.CreateAndStoreSecretOfLife,
+		AssertResourceEncryptedFunc:    operatorencryption.AssertSecretOfLifeEncrypted,
+		AssertResourceNotEncryptedFunc: operatorencryption.AssertSecretOfLifeNotEncrypted,
+		ResourceFunc:                   operatorencryption.SecretOfLife,
+		ResourceName:                   "SecretOfLife",
+		EncryptionProvider:             configv1.EncryptionTypeKMS,
+	})
 }
