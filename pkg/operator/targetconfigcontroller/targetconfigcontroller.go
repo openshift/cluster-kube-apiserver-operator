596391a3d6b8c34af302badc2d6cece6c71472b8
Fabio Bertinatto <fbertina@redhat.com>
2026-02-25 13:55:58 -0300

    wip

--- pkg/operator/targetconfigcontroller/targetconfigcontroller.go
+++ pkg/operator/targetconfigcontroller/targetconfigcontroller.go
@@ -330,13 +330,9 @@ func managePods(ctx context.Context, client coreclientv1.ConfigMapsGetter, featu
 		required.Spec.Containers[i].Env = append(container.Env, proxyEnvVars...)
 	}
 
-	if err := addKMSPluginVolumeAndMountToPodSpec(&required.Spec, "kube-apiserver"); err != nil {
-		return nil, false, fmt.Errorf("failed to add KMS encryption volumes: %w", err)
-	}
-
-	// TODO: this will only be accessible to those who have read permissions! Feel free to ask me if you need access
-	kmsPluginImage := "quay.io/bertinatto/vault:latest"
-	if err := addKMSPluginSidecar(&required.Spec, kmsPluginImage, secretLister, operatorclient.TargetNamespace); err != nil {
+	// TODO: placeholder. I (fbertina) need to grant you read permissions. Please ask
+	kmsPluginImage := "quay.io/bertinatto/vault:v1"
+	if err := AddKMSPluginToPodSpec(&required.Spec, featureGateAccessor, secretLister, operatorclient.TargetNamespace, kmsPluginImage); err != nil {
 		return nil, false, fmt.Errorf("failed to add KMS plugin sidecar: %w", err)
 	}
 
