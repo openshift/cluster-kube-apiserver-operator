772b251c2e1e6a4bf38c08df9a1db104074012ea
Arda Güçlü <aguclu@redhat.com>
2026-04-22 10:44:03 +0300

    Fake bump library-go to test new KMS data types

--- vendor/github.com/openshift/library-go/pkg/operator/encryption/state/types.go
+++ vendor/github.com/openshift/library-go/pkg/operator/encryption/state/types.go
@@ -40,8 +40,20 @@ type KeyState struct {
 	InternalReason string
 	// the user via unsupportConfigOverrides.encryption.reason triggered this key.
 	ExternalReason string
-	// Encoded KMSEncryptionConfig that stores the KMS related fields
-	KMSEncryptionConfig *apiserverconfigv1.KMSConfiguration
+	// stores the all KMS encryption mode related configurations
+	KMS *KMSConfig
+}
+
+// KMSConfig stores all KMS encryption mode related configurations
+type KMSConfig struct {
+	// Encoded EncryptionConfig that stores the KMS related fields
+	EncryptionConfig *apiserverconfigv1.KMSConfiguration
+}
+
+// EncryptionSecretData stores the EncryptionConfiguration and per-key KMS configurations keyed by keyID.
+type EncryptionSecretData struct {
+	EncryptionConfig *apiserverconfigv1.EncryptionConfiguration
+	KMSConfig        map[string]*KMSConfig
 }
 
 type MigrationState struct {
