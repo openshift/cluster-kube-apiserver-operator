d314dc3783a44bb9c9ef72f73a25881c2e6a146f
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2026-07-03 08:53:52 +0200

    testKMSEncryptionProvidersMigration

--- test/e2e-encryption-kms/encryption_kms.go
+++ test/e2e-encryption-kms/encryption_kms.go
@@ -8,6 +8,8 @@ import (
 
 	g "github.com/onsi/ginkgo/v2"
 
+	"k8s.io/apimachinery/pkg/runtime"
+
 	"github.com/openshift/cluster-kube-apiserver-operator/pkg/operator/operatorclient"
 	operatorencryption "github.com/openshift/cluster-kube-apiserver-operator/test/library/encryption"
 	library "github.com/openshift/library-go/test/library/encryption"
@@ -63,24 +65,47 @@ func testKMSEncryptionOnOff(ctx context.Context, t testing.TB) {
 // 4. Migrates between the providers in the shuffled order
 // 5. Verifies secret is correctly encrypted after each migration
 func testKMSEncryptionProvidersMigration(ctx context.Context, t testing.TB) {
-	library.TestEncryptionProvidersMigration(ctx, t, library.ProvidersMigrationScenario{
-		BasicScenario: library.BasicScenario{
-			Namespace:                       operatorclient.GlobalMachineSpecifiedConfigNamespace,
-			LabelSelector:                   "encryption.apiserver.operator.openshift.io/component" + "=" + operatorclient.TargetNamespace,
-			EncryptionConfigSecretName:      fmt.Sprintf("encryption-config-%s", operatorclient.TargetNamespace),
-			EncryptionConfigSecretNamespace: operatorclient.GlobalMachineSpecifiedConfigNamespace,
-			OperatorNamespace:               operatorclient.OperatorNamespace,
-			TargetGRs:                       operatorencryption.DefaultTargetGRs,
-			AssertFunc:                      operatorencryption.AssertSecretsAndConfigMaps,
+	providers := library.ShuffleEncryptionProviders([]library.EncryptionProvider{
+		librarykms.DefaultVaultEncryptionProvider(ctx, t),
+		library.SupportedStaticEncryptionProviders[rand.IntN(len(library.SupportedStaticEncryptionProviders))],
+	})
+
+	library.TestEncryptionProvidersMigration(ctx, t, []library.ProvidersMigrationScenario{
+		{
+			BasicScenario: library.BasicScenario{
+				Namespace:                       operatorclient.GlobalMachineSpecifiedConfigNamespace,
+				LabelSelector:                   "encryption.apiserver.operator.openshift.io/component" + "=" + operatorclient.TargetNamespace,
+				EncryptionConfigSecretName:      fmt.Sprintf("encryption-config-%s", operatorclient.TargetNamespace),
+				EncryptionConfigSecretNamespace: operatorclient.GlobalMachineSpecifiedConfigNamespace,
+				OperatorNamespace:               operatorclient.OperatorNamespace,
+				TargetGRs:                       library.KASTargetGRs,
+				AssertFunc:                      library.AssertSecretsAndConfigMaps,
+			},
+			CreateResourceFunc:             library.CreateAndStoreSecretOfLife,
+			AssertResourceEncryptedFunc:    library.AssertSecretOfLifeEncrypted,
+			AssertResourceNotEncryptedFunc: library.AssertSecretOfLifeNotEncrypted,
+			ResourceFunc:                   library.SecretOfLife,
+			ResourceName:                   "SecretOfLife",
+			EncryptionProviders:            providers,
 		},
-		CreateResourceFunc:             operatorencryption.CreateAndStoreSecretOfLife,
-		AssertResourceEncryptedFunc:    operatorencryption.AssertSecretOfLifeEncrypted,
-		AssertResourceNotEncryptedFunc: operatorencryption.AssertSecretOfLifeNotEncrypted,
-		ResourceFunc:                   operatorencryption.SecretOfLife,
-		ResourceName:                   "SecretOfLife",
-		EncryptionProviders: library.ShuffleEncryptionProviders([]library.EncryptionProvider{
-			librarykms.DefaultVaultEncryptionProvider(ctx, t),
-			library.SupportedStaticEncryptionProviders[rand.IntN(len(library.SupportedStaticEncryptionProviders))],
-		}),
+		{
+			BasicScenario: library.BasicScenario{
+				Namespace:                       "openshift-config-managed",
+				LabelSelector:                   "encryption.apiserver.operator.openshift.io/component" + "=" + "openshift-oauth-apiserver",
+				EncryptionConfigSecretName:      "encryption-config-openshift-oauth-apiserver",
+				EncryptionConfigSecretNamespace: "openshift-config-managed",
+				OperatorNamespace:               "openshift-authentication-operator",
+				TargetGRs:                       library.AuthTargetGRs,
+				AssertFunc:                      library.AssertTokens,
+			},
+			CreateResourceFunc: func(t testing.TB, clientSet library.ClientSet, _ string) runtime.Object {
+				return library.CreateAndStoreTokenOfLife(context.TODO(), t, clientSet)
+			},
+			AssertResourceEncryptedFunc:    library.AssertTokenOfLifeEncrypted,
+			AssertResourceNotEncryptedFunc: library.AssertTokenOfLifeNotEncrypted,
+			ResourceFunc:                   library.TokenOfLife,
+			ResourceName:                   "TokenOfLife",
+			EncryptionProviders:            providers,
+		},
 	})
 }
